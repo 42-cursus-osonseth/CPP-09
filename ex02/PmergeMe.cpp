dd52c2f81fc8ef6cfc346089cb77ce933000fb0f
Osonseth <max.cesaria@gmail.com>
2025-01-26 11:40:44 +0100

    ex02 first push

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -10,7 +10,7 @@ PmergeMe::PmergeMe(char **argv)
         long int n;
         while (iss >> n)
             vec.push_back(n);
-        if ((iss.fail() && !iss.eof()) || n < 0 || n > INT_MAX)
+        if ((iss.fail() && !iss.eof()) || n < 0 || n > INT_MAX || !argv[i][0])
             throw std::invalid_argument("Error: Invalid arguments : " + std::string(argv[i]));
         
     }
